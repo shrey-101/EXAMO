2373a20f31660734dc642f2cc5923c384ab27cdd
SAURABH YADAV <75905524+sy425191@users.noreply.github.com>
2021-05-22 16:33:13 +0530

    Delete Chrome extension directory

--- Chrome extension/popup.js
+++
@@ -1,8 +0,0 @@
-$("#submit").click(function(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost/ext/index.php", true);
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      alert('This is the response from the server: '+ xhr.responseText );
-    }
-}})
